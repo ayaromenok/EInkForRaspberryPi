70368cf81ac514ac8d60ded0198fb817626133dc
Andrey Yaromenok <ayaromenok@gmail.com>
2020-06-18 09:29:32 +0300

    #1 added UI creation

--- apps/PDFAppForRPi/mainwindow.cpp
+++ apps/PDFAppForRPi/mainwindow.cpp
@@ -1,12 +1,28 @@
 #include "mainwindow.h"
 
+#include <QDebug>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QPdfDocument>
+#include <QPdfView>
+
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+    : QWidget(parent)
 {
+    setupUI();
     setGeometry(0, 0, 1404/2, 1872/2);
 }
 
 MainWindow::~MainWindow()
 {
+}
+
+void
+MainWindow::setupUI()
+{
+    QVBoxLayout *lo = new QVBoxLayout();
+    QLabel *lbIp4 = new QLabel("QLabel");
+    lo->addWidget(lbIp4);
+    this->setLayout(lo);
 }
 
--- apps/PDFAppForRPi/mainwindow.h
+++ apps/PDFAppForRPi/mainwindow.h
@@ -1,14 +1,22 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
+#include <QWidget>
+class QPdfDocument;
+class QPdfView;
 
-class MainWindow : public QMainWindow
+class MainWindow : public QWidget
 {
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private:
+    void setupUI();
+
+    QPdfDocument*       _pdfDoc;
+    QPdfView*           _pdfView;
 };
 #endif // MAINWINDOW_H
